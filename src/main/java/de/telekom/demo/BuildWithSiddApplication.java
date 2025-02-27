53969d18c02b8ce475487a1375665599a962dec5
Sidd <scorpion.sem@gmail.com>
2025-02-28 01:44:31 +0530

    Initial Commit

---
+++ src/main/java/de/telekom/demo/BuildWithSiddApplication.java
@@ -0,0 +1,13 @@
+package de.telekom.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildWithSiddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildWithSiddApplication.class, args);
+    }
+
+}
